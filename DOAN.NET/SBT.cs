8a432d7f80c94fcdc17c62ab66f88655f9775ad2
Hoang992000 <buivuhoang12345@gmail.com>
2021-04-23 23:58:49 +0700

    first commit

---
+++ DOAN.NET/SBT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DOAN.NET
+{
+    class SBT
+    {
+        public const string con_str = "Data Source=DESKTOP-A8BINMD;Initial Catalog=QLDV_DoAn.NET;Integrated Security=True";
+        public static SqlConnection conn = new SqlConnection(con_str);
+    }
+}
